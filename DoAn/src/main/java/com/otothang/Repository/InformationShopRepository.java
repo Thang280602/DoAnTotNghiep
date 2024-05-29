38e8dece100bf788d27fb2ca8ea7f0a92145991d
Thang280602 <thang280602@gmail.com>
2024-05-30 06:41:59 +0700

    Update

--- DoAn/src/main/java/com/otothang/Repository/InformationShopRepository.java
+++
@@ -1,9 +0,0 @@
-package com.otothang.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.otothang.models.InformationShop;
-
-public interface InformationShopRepository extends JpaRepository<InformationShop, Integer>{
-
-}
--- DoAn/src/main/resources/static/js/config.js
+++
@@ -1,4 +0,0 @@
-var socket = new SockJS('/ws');
-stompClient = Stomp.over(socket);
-
-stompClient.connect({}, onConnected, onError);
